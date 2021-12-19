58239e6abfb391cdc3edacc6a9f2c07c9ecb7857
John de Graaff <john@de-graaff.net>
2021-12-19 14:31:45 -0800

    'edit bin/gcm.sh'

--- bin/gcm.sh
+++ bin/gcm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #MSG="$1"
 MSG="$@" # note: don't use: $*
-echo "# > git commit -am \"$MSG\" ... "
-git commit -am "$MSG"
+echo "# > git commit -am \"'$MSG'\" ... "
+git commit -am "'$MSG'"
 #
