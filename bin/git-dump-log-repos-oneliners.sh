1ca7d7c087eb93fa0cebb566c2b1c4cde72ece64
John de Graaff <john@de-graaff.net>
2022-02-14 22:07:03 +0100

    'add bin/month-to-since-before.pl bin/git-dump-log-repos-oneliners.sh'

---
+++ bin/git-dump-log-repos-oneliners.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#= git-dump-log-repos-oneliners.sh 
+# 2022(c) John@de-Graaff.net
+
+MONTH=$1
+echo "# month = $MONTH"
+
+# MY_ARGS=$( ./month-to-since-before.pl $1 )
+MY_ARGS=$( month-to-since-before.pl $1 )
+# echo "${MY_ARGS}" ; exit
+
+# idea from: https://stackoverflow.com/questions/1469849/how-to-split-one-string-into-multiple-strings-separated-by-at-least-one-space-in
+# sentence="this is a story"
+# stringarray=($sentence)
+# echo ${stringarray[0]}
+
+ARG_ARRAY=($MY_ARGS)
+SINCE=${ARG_ARRAY[0]}
+BEFORE=${ARG_ARRAY[1]}
+echo "# calc => SINCE=$SINCE BEFORE=$BEFORE"
+
+for REPO in TokenMe-* SUM4 ; do 
+  # echo "REPO='$REPO'" 
+  # FILE="git-log-oneline-numstat--repo--$REPO--since-$SINCE--before-$BEFORE"
+  FILE="git-log-oneline-numstat--repo--$REPO--month-$MONTH"
+  # echo "FILE='$FILE'" 
+  #
+  TXT="$FILE.txt"
+  # echo "TXT='$TXT'" 
+  #
+  cd $REPO
+  echo "# > git log --oneline --all --pretty=format:'\"epoch:%at\",\"time:%ad\",\"repo:REPONAME\",\"abbrev-hash:%h\",\"committer:%ae\",\"subject:%s\"' --numstat --since=\"$SINCE\" --before=\"$BEFORE\" > ../$TXT"
+  git log --oneline --all --pretty=format:'"epoch:%at","time:%ad","repo:REPONAME","abbrev-hash:%h","committer:%ae","subject:%s"' --numstat --since="$SINCE" --before="$BEFORE" > ../$TXT
+  cd -
+  #
+  # -s == size, so if >0 then not empty:
+  if [ -s $TXT ]; then
+    #
+    SMASH="${FILE}-smash.csv"
+    # echo "SMASH='$SMASH'" 
+    #
+    echo "# smashing ..."
+    cat $TXT | sed "s/REPONAME/$REPO/" | perl -e 'my $LAST="", $ADD="", $REM="", $FILE=""; while (my $LINE=<STDIN>) { chomp($LINE); next if ($LINE eq ""); if ($LINE =~ /^"/) {$LAST=$LINE; ($ADD,$REM,$FILE) = ("","",""); } else {($ADD,$REM,$FILE) = split /\s/, $LINE; print "$LAST,\"added:$ADD\",\"removed:$REM\",\"file:$FILE\"\n"; } }' > $SMASH
+    #
+  fi
+  rm $TXT
+  #
+  echo "# done. "
+  echo
+  #
+done
+
+#-EOF
+
---
+++ bin/month-to-since-before.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+#= month-to-since-before.pl
+# 2022(c) John@de-Graaff.net 
+
+# Usage: 
+# > ./month-to-since-before.pl 2022-02
+# 2022-01-30 2022-02-28
+
+#NOT: Usage: 
+# > ./month-to-since-before.pl 2022-02
+# 2022-01-31 2022-03-01
+
+# debian: > sudo apt install -y libdatetime-perl
+use DateTime;
+
+my $arg1 = join "-", @ARGV;
+if ($arg1 eq "") { $arg1 = "1970-01"; }
+
+#print "arg1 = $arg1 \n"; exit;
+
+#my( $year, $month ) = qw( 2012 2 );
+my( $year, $month ) = split /\-/, $arg1;
+
+my $date_before = DateTime->new(
+  year  =>  $year,
+  month => $month,
+  day   => 1,
+);
+
+$date_before->add( months => 1 )->subtract( days => 1 );
+
+my $date_since = $date_before->clone;
+
+# $date_since->add( months => 1 )->subtract( days => 1 );
+$date_since->subtract( months => 1 )->subtract( days => 1 );
+
+# print "date_since=" .  $date_since->ymd('-')  . "\n";
+# print "date_before=" . $date_before->ymd('-') . "\n";
+
+print $date_since->ymd('-') . " " . $date_before->ymd('-') . "\n";
+
+#-EOF
+
