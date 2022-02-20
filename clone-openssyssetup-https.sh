beda04952d84d8a40fb742b58365796f21361458
John de Graaff <john@de-graaff.net>
2022-02-20 16:03:17 +0100

    'add clone-openssyssetup-https.sh clone-openssyssetup-ssh.sh'

---
+++ clone-openssyssetup-https.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+curl https://j71.nl/oss-https.sh | bash
+
+## #!/bin/bash
+## # Usage:
+## # if you want to make direct changes ==> curl https://j71.nl/oss-ssh.sh   | bash 
+## # if you want to make it read-only*  ==> curl https://j71.nl/oss-https.sh | bash 
+## #                                 or ==> curl https://j71.nl/oss          | bash 
+## # (*) you can still make changes in a temporary branch, push using https, and use a access-token (purpose-pwd), or commit to new branch and create a pull-request
+## #
+## cd $HOME
+## git clone https://github.com/jdg71nl/opensyssetup.git
+## ./opensyssetup/setup.sh
+## #-EOF
+
+#-EOF
---
+++ clone-openssyssetup-ssh.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+curl https://j71.nl/oss-ssh.sh | bash
+
+## #!/bin/bash
+## # Usage:
+## # if you want to make direct changes ==> curl https://j71.nl/oss-ssh.sh   | bash 
+## # if you want to make it read-only*  ==> curl https://j71.nl/oss-https.sh | bash 
+## #                                 or ==> curl https://j71.nl/oss          | bash 
+## # (*) you can still make changes in a temporary branch, push using https, and use a access-token (purpose-pwd), or commit to new branch and create a pull-request
+## #
+## mkdir -pv $HOME/.ssh && chmod 0700 $HOME/.ssh
+## if [ ! -f $HOME/.ssh/id_rsa ] ; then echo "# Creating _new_ SSH-keypair ..." ; date > $HOME/.ssh/id_rsa.generate.txt ; ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -P '' >> $HOME/.ssh/id_rsa.generate.txt; fi
+## echo "# copy this key to your Github account my-SSH-keys ==> cat \$HOME/.ssh/id_rsa.pub"
+## echo "# "
+## cat $HOME/.ssh/id_rsa.pub 
+## echo "# "
+## #
+## echo "# hit <ENTER> if Github-stuff is done, else <CTRL>-C ..."
+## read
+## #
+## cd $HOME
+## git clone git@github.com:jdg71nl/opensyssetup.git
+## ./opensyssetup/setup.sh
+## #-EOF
+
+#-EOF
