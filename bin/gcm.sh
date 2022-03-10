c9cb69a3ae5003d807de1be4978a5b36eac3c8e2
John de Graaff <john@de-graaff.net>
2022-03-10 08:44:35 +0100

    edit bin/gcm.sh

--- bin/gcm.sh
+++ bin/gcm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#= gcm.sh
 #MSG="$1"
 #MSG="$@" # note: don't use: $*
 #git commit -am "'$MSG'"
 # https://stackoverflow.com/questions/1668649/how-to-keep-quotes-in-bash-arguments
+echo "# NOTE: use \"double-quotes\" around the commit-message, like: gcm.sh \"my commit msg with <any> special chars like $.\"  ..! "
 echo "# > git commit -am \"$@\" ... "
 git commit -am "$@" 
 #
