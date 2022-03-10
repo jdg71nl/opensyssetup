2929d1e596138d97c19541837a635834bdcc0e46
John de Graaff <john@de-graaff.net>
2022-03-10 08:41:47 +0100

    some msg (test) - and then > &gt;

--- bin/gcm.sh
+++ bin/gcm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #MSG="$1"
-MSG="$@" # note: don't use: $*
-echo "# > git commit -am \"'$MSG'\" ... "
-git commit -am "'$MSG'"
+#MSG="$@" # note: don't use: $*
+#git commit -am "'$MSG'"
+# https://stackoverflow.com/questions/1668649/how-to-keep-quotes-in-bash-arguments
+echo "# > git commit -am \"$@\" ... "
+git commit -am "$@" 
 #
