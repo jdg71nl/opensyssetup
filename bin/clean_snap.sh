08c2ea409a6878d6460ee75f79c34dbfe3e7dff5
John de Graaff <john@de-graaff.net>
2022-03-25 09:20:49 +0100

    add bin/clean_snap.sh

---
+++ bin/clean_snap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://www.debugpoint.com/2021/03/clean-up-snap/
+#Removes old revisions of snaps
+#CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' | while read snapname revision; do
+  snap remove "$snapname" --revision="$revision"
+done
+#-eof
