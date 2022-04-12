e60708e1becb07790042a55a99fa54ed4873ce8e
John de Graaff <john@de-graaff.net>
2022-04-12 14:41:13 +0200

    some

--- bin/rename_allfiles_space_to_dash.sh
+++ bin/rename_allfiles_space_to_dash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+find . -type f -exec rename_file_pattern.sh "{}" 's/[ ]+-[ ]+/-/g' \; 
 find . -type f -exec rename_file_pattern.sh "{}" 's/[ ]+/-/g' \; 
+find . -type f -exec rename_file_pattern.sh "{}" 's/[\-]{3,}/--/g' \; 
 exit 0
 
