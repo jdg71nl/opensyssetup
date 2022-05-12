b419769fd2ecfe1f16828773d361cdbcc1403f76
John de Graaff <john@de-graaff.net>
2022-05-12 16:25:48 +0200

    update: bin/curl-show-headers.sh

--- bin/curl-show-headers.sh
+++ bin/curl-show-headers.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "# cmd> curl -v -s -o - -X OPTIONS -H \"Custom-Test-Header: So this is a test header.\" $1"
-curl -v -s -o - -X OPTIONS -H "Custom-Test-Header: So this is a test header." $1
+#= curl-show-headers.sh
+#
+#echo "# cmd> curl -v -s -o - -X OPTIONS -H \"Custom-Test-Header: So this is a test header.\" $1"
+#curl -v -s -o - -X OPTIONS -H "Custom-Test-Header: So this is a test header." $1
+#
+echo "# cmd> curl -v -s -o - $1"
+curl -v -s -o - $1
+#
+#-eof
 
