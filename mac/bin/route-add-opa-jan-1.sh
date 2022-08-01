f7c78c864c3f5bd4b33a7f34bba6a2648dc1ca31
John de Graaff <john@de-graaff.net>
2022-08-01 14:15:39 +0200

    add mac/bin/route-add-opa-jan-1.sh

---
+++ mac/bin/route-add-opa-jan-1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#BASENAME=`basename $0`
+#usage() {
+#  echo "# usage: $BASENAME { auto | [ -y YYYY ] [ -d MMDD ] [-h HHMM ] [-s SS] } " 1>&2 
+#  exit 1
+#}
+#
+MYID=$( id -u )
+if [ $MYID != 0 ]; then
+  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 1 ;
+fi
+#
+echo "# > /sbin/route add 10.220.0.0/16 10.77.66.1 "
+/sbin/route add 10.220.0.0/16 10.77.66.1
+echo "# > /sbin/route add 10.21.0.0/16 10.77.66.1 "
+/sbin/route add 10.21.0.0/16 10.77.66.1
+#
+echo "# "
+echo "# show route table on MacOS: "
+echo "# > sudo netstat -nr -f inet "
+echo "# "
+echo "# remote these routes: "
+echo "# > sudo /sbin/route delete 10.220.0.0/16 10.77.66.1 "
+echo "# > sudo /sbin/route delete 10.21.0.0/16 10.77.66.1 "
+echo "# "
+#
+exit 0
+#
