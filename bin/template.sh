95d718e7970b3defd6466ed3c5e3ab6f8db883fe
John de Graaff <john@de-graaff.net>
2022-10-16 02:10:41 +0200

    edit bin/template.sh

--- bin/template.sh
+++ bin/template.sh
@@ -2,9 +2,11 @@
 #= template.sh | updated: d220705
 # (c)2022 John@de-Graaff.net
 #
-#BASENAME=`basename $0`
-#SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
-#SCRIPT_PATH=`dirname $SCRIPT`
+BASENAME=`basename $0`
+echo "# running: $BASENAME ... "
+SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
+SCRIPT_PATH=`dirname $SCRIPT`
+#
 #MYUID=$( id -u )
 #usage() {
 #  #echo "# usage: $BASENAME { req.flag | [ -opt.flag string ] } " 1>&2 
@@ -14,10 +16,12 @@
 #if [ $MYUID != 0 ]; then
 #  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 1 ;
 #fi
+#
 #if [ -f ${SOME_FILE} ]; then
 #else
 #fi
 #
+# my statements here ...
 #
 exit 0
 #
