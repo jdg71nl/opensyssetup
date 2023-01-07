17aa479dd2d891c7ecd32f78e542e0801a07e007
John de Graaff <john@de-graaff.net>
2023-01-07 13:07:38 +0100

    some

--- bin/config_replace_match_with_line.sh
+++ bin/config_replace_match_with_line.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#
 # sudo config_replace_match_with_line.sh /etc/sysctl.conf 'fs.inotify.max_user_watches' 'fs.inotify.max_user_watches=524288'
+#
+# https://www.sudo.ws/security/advisories/secure_path/
+# # add sith visudo:
+# Defaults editor=/usr/bin/vim
+# Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/opensyssetup/bin"
 #
 FILE="$1"
 MATCH="$2"
@@ -12,6 +18,9 @@ fi
 #
 echo "# exec> perl -pi -e 's/^$MATCH.*$/$LINE/' $FILE "
 perl -pi -e "'s/^$MATCH.*$/$LINE/'" $FILE 
+#
+echo "# exec> grep '$MATCH' $FILE "
+grep "'$MATCH'" $FILE 
 #
 #-eof
 
