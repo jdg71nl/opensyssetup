e59659ad572829de6a182e60a34c98a666b37c51
John de Graaff <john@de-graaff.net>
2023-03-21 11:15:41 +0100

    some

--- bin/rsync_this.sh
+++ bin/rsync_this.sh
@@ -3,12 +3,12 @@
 #set -o xtrace
 #
 # edit this:
-SRC="/Users/jdg/Dropbox/FAIRFLOW/Incorporation/Restructure-2022/web/"
-FOLDER="www/dgt-bv.com/webroot/code/28055838/fairflow-dgp-restructure-2022/"
+SRC="/Users/jdg/Dropbox/JDGPRIVE/LB21/riool-breuk/"
+FOLDER="/home/jdg/www/john.de-graaff.net/webroot/code/63097749/riool-breuk/"
 #
 SSH="jdg@jwww.j71.nl"
 DST="$SSH:$FOLDER"
 echo "# > rsync -v -rtlz --delete --delete-excluded --exclude='.DS_Store' -e 'ssh -p 2221' $SRC $DST "
-rsync -v -rtlz --delete --delete-excluded --exclude='.DS_Store' -e 'ssh -p 2221' $SRC $DST
+rsync -v -rtlz --delete --delete-excluded --exclude='.DS_Store' --exclude='rsync_this.sh' -e 'ssh -p 2221' $SRC $DST
 #-eof
 
