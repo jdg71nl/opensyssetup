34b4333053330cdbcdbfd9784cb9ebc018b08e37
John de Graaff <john@de-graaff.net>
2023-04-24 16:04:57 +0200

    some

---
+++ mac/bin/ping_audible_interval1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#ping -Ani1 1.0.0.1
+#ping -A -n -i1 1.0.0.1
+echo "# > ping -A -n -i1 1.0.0.1 "
+ping -A -n -i1 1.0.0.1
+#
--- mac/bin/ping_audible_interval3.sh
+++ mac/bin/ping_audible_interval3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#ping -Ani3 1.0.0.1
 #ping -A -n -i3 1.0.0.1
-ping -Ani3 1.0.0.1
+echo "# > ping -A -n -i3 1.0.0.1 "
+ping -A -n -i3 1.0.0.1
 #
