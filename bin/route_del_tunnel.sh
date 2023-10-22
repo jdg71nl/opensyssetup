d58a3afa7b9acb48aed0e97889d0d9173b4f3981
John de Graaff <john@de-graaff.net>
2023-10-22 21:04:30 +0200

    some

--- bin/route_del_tunnel.sh
+++ bin/route_del_tunnel.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#= route_del_tunnel.sh | updated: d230928
+# (c)2023 John@de-Graaff.net
+#
+# display every line executed in this bash script:
+#set -o xtrace
+#
+BASENAME=`basename $0`
+echo "# running: $BASENAME ... "
+SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
+SCRIPT_PATH=`dirname $SCRIPT`
+#
+MYUID=$( id -u )
+#usage() {
+#  #echo "# usage: $BASENAME { req.flag | [ -opt.flag string ] } " 1>&2 
+#  echo "# usage: $BASENAME " 1>&2 
+#  exit 1
+#}
+if [ $MYUID != 0 ]; then
+  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 1 ;
+fi
+#
+#
 
 ## examples of /sbin/ip :
 # /sbin/ip route add 193.70.40.1 dev eth0
@@ -49,3 +71,4 @@
 /sbin/ip route
 
 #-eof
+
