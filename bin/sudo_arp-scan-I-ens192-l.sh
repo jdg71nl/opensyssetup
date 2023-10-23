50634e67155618b02f95f5842c683dc3c79b496d
John de Graaff <john@de-graaff.net>
2023-10-23 11:03:17 +0200

    some

---
+++ bin/sudo_arp-scan-I-ens192-l.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#= sudo_arp-scan-I-ens192-l.sh | updated: d231023
+# (c)2023 John@de-Graaff.net
+#
+# display every line executed in this bash script:
+#set -o xtrace
+#
+BASENAME=`basename $0`
+echo "# running: $BASENAME ... "
+SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
+SCRIPT_PATH=`dirname $SCRIPT`
+#
+MYUID=$( id -u )
+#usage() {
+#  #echo "# usage: $BASENAME { req.flag | [ -opt.flag string ] } " 1>&2 
+#  echo "# usage: $BASENAME " 1>&2 
+#  exit 1
+#}
+if [ $MYUID != 0 ]; then
+  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 1 ;
+fi
+#
+#
+arp-scan -I ens192 -l
+#
+#
+exit 0
+#
+#-eof
