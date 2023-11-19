f1be88a9533cb44ac22a6a52ccb44f285246ea35
John de Graaff <john@de-graaff.net>
2023-11-19 12:43:57 +0100

    some

--- jdg-prefs-apt.sh
+++ jdg-prefs-apt.sh
@@ -25,8 +25,41 @@ fi
 #fi
 #
 #
-apt install -y git curl htop lsof vim minicom netcat arp-scan arping fping jq ntp openvpn rsync sudo wget locate watchdog
+OS=""
+DIST=""
+if [ -e /etc/debian_version ]; then
+  #
+  #OS=$(cat /etc/issue | head -1 | awk '{ print tolower($1) }')
+  OS=$(cat /etc/issue | head -1 | awk '{ print $1 }')
+  #
+  # some Debians have jessie/sid in their /etc/debian_version
+  # while others have '6.0.7'
+  if grep -q '/' /etc/debian_version; then
+    DIST=$(cut --delimiter='/' -f1 /etc/debian_version)
+  else
+    DIST=$(cut --delimiter='.' -f1 /etc/debian_version)
+  fi
+fi
+echo "# OS='$OS' DIST='$DIST' "
+if [ $OS != "Debian" ]; then
+  echo "# Error: detected non-Debian, so exit ..."
+  exit 1
+fi
 #
+#
+if [ $DIST == "12" ]; then
+  echo "# > apt install -y git curl htop lsof vim minicom netcat-openbsd arp-scan arping fping jq ntp openvpn rsync sudo wget locate watchdog ... "
+            apt install -y git curl htop lsof vim minicom netcat-openbsd arp-scan arping fping jq ntp openvpn rsync sudo wget locate watchdog
+else
+  echo "# > apt install -y git curl htop lsof vim minicom netcat         arp-scan arping fping jq ntp openvpn rsync sudo wget locate watchdog ... "
+            apt install -y git curl htop lsof vim minicom netcat         arp-scan arping fping jq ntp openvpn rsync sudo wget locate watchdog
+fi
+#
+# d231119 changes:
+# Package netcat is a virtual package provided by:
+#  netcat-openbsd 1.219-1
+#  netcat-traditional 1.10-47
+# You should explicitly select one to install.
 # 
 ## also (manual?): 
 #
