f06d144c08e0d6376736bf3f637edbab471f10ad
John de Graaff <john@de-graaff.net>
2023-12-09 21:55:57 +0100

    some

---
+++ rpi_setup/rpi_touch_screen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://www.raspberrypi.com/documentation/accessories/display.html
+# add this section on the first line in this file:
+# vi /boot/firmware/cmdline.txt 
+# video=DSI-1:800x480@60,rotate=180
+
+#-eof
+
