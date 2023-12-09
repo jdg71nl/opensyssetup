6cf3751febcf44740d86ac6fafa76add842b3b7d
John de Graaff <john@de-graaff.net>
2023-12-09 17:05:36 +0100

    some

--- bin/template_sudo.sh
+++ bin/template_sudo.sh
@@ -11,11 +11,19 @@ SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand 
 SCRIPT_PATH=`dirname $SCRIPT`
 #
 MYUID=$( id -u )
+#
 #usage() {
 #  #echo "# usage: $BASENAME { req.flag | [ -opt.flag string ] } " 1>&2 
 #  echo "# usage: $BASENAME " 1>&2 
 #  exit 1
 #}
+#
+# https://unix.stackexchange.com/questions/129072/whats-the-difference-between-and
+# According to this page, $@ and $* do pretty much the same thing:
+# - The $@ holds list of all arguments passed to the script. 
+# - The $* holds list of all arguments passed to the script.
+# They aren't the same. $* is a single string, whereas $@ is an actual array.
+#
 if [ $MYUID != 0 ]; then
   echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 1 ;
 fi
