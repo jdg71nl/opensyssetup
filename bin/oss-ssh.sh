1c760b77541b669f906c1b94350739bffd200309
John de Graaff <john@de-graaff.net>
2023-12-17 14:17:25 +0100

    some

--- bin/oss-ssh.sh
+++ bin/oss-ssh.sh
@@ -6,24 +6,24 @@
 # (*) you can still make changes in a temporary branch, push using https, and use a access-token (purpose-pwd), or commit to new branch and create a pull-request
 #
 #
-# - - - - - - = = = - - - - - - . - - - - - - = = = - - - - - - .
-f_echo_exit1() { echo $1 ; exit 1 ; }
-if [ ! -e /etc/debian_version ]; then f_echo_exit1 "# Error: found non-Debain OS .." ; fi
-if ! which sudo >/dev/null ; then f_echo_exit1 "# please install first (as root) ==> apt install sudo ";; fi
-if ! which dpkg-query >/dev/null ; then f_echo_exit1 "# please install first: using ==> sudo apt install dpkg-query "; fi
-# - - - - - - = = = - - - - - - . - - - - - - = = = - - - - - - .
+ #-- # - - - - - - = = = - - - - - - . - - - - - - = = = - - - - - - .
+ #-- f_echo_exit1() { echo $1 ; exit 1 ; }
+ #-- if [ ! -e /etc/debian_version ]; then f_echo_exit1 "# Error: found non-Debain OS .." ; fi
+ #-- if ! which sudo >/dev/null ; then f_echo_exit1 "# please install first (as root) ==> apt install sudo " ; fi
+ #-- if ! which dpkg-query >/dev/null ; then f_echo_exit1 "# please install first: using ==> sudo apt install dpkg-query " ; fi
+ #-- # - - - - - - = = = - - - - - - . - - - - - - = = = - - - - - - .
+ #-- f_check_install_packages() { 
+ #--   for PKG in $@ ; do 
+ #--     if ! dpkg-query -l $PKG >/dev/null ; then 
+ #--       echo "# auto installing package '$PKG' ==> sudo apt install -y $PKG " 
+ #--       sudo apt install -y $PKG 
+ #--     fi
+ #--   done
+ #-- }
+ #-- f_check_install_packages curl git sudo
+ #-- # - - - - - - = = = - - - - - - . - - - - - - = = = - - - - - - .
 #
 cd $HOME
-#
-f_check_install_packages() { 
-  for PKG in $@ ; do 
-    if ! dpkg-query -l $PKG >/dev/null ; then 
-      echo "# auto installing package '$PKG' ==> sudo apt install -y $PKG " 
-      sudo apt install -y $PKG 
-    fi
-  done
-}
-f_check_install_packages curl git sudo
 #
 if [ ! -f $HOME/.ssh/id_rsa ] ; then
   echo "# first create a local SSH key: "
