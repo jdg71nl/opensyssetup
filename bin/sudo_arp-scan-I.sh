ce4537ffcee9977e7ce985ae3e601b0982bc4ae1
John de Graaff <john@de-graaff.net>
2024-01-08 23:53:19 +0100

    some

---
+++ bin/sudo_arp-scan-I.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#= sudo_arp-scan-I.sh
+# (c)2023 John@de-Graaff.net
+#
+# display every line executed in this bash script:
+#set -o xtrace
+#
+BASENAME=`basename $0`
+echo "# running: $BASENAME ... "
+SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
+SCRIPT_PATH=`dirname $SCRIPT`
+#
+MYUID=$( id -u )
+#usage() {
+#  #echo "# usage: $BASENAME { req.flag | [ -opt.flag string ] } " 1>&2 
+#  echo "# usage: $BASENAME " 1>&2 
+#  exit 1
+#}
+if [ $MYUID != 0 ]; then
+  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 1 ;
+fi
+#
+#
+IFACE="$1"
+#
+arp-scan -I $IFACE -l
+#
+#
+exit 0
+#
+#-eof
