763b096e6f967e9c9b48845dc54a49dd5dc0c14b
John de Graaff <john@de-graaff.net>
2024-01-21 21:13:09 +0100

    some

--- colorprompt.sh
+++ colorprompt.sh
@@ -29,6 +29,14 @@ NOCOLOR='\e[0m'              # No Color
 
 export PATH=$HOME/opensyssetup/bin:$HOME/opensyssetup/mac/bin:$PATH
 
+# https://code.visualstudio.com/docs/setup/mac
+#
+# --[CWD=~/opensyssetup(git:main)]--[1705867071 20:57:51 Sun 21-Jan-2024 CET]--[jdg@MB18-jdg71nl]--[os:MacOS--13.6.3,isa:x86_64]------
+# > lt /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code
+# -rwxr-xr-x  1 jdg  staff   1.0K Jan 18 15:28 /Applications/Visual Studio Code.app/Contents/Resources/app/bin/code
+#
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
 # jdg-tokenme:
 alias sti='sudo -u tokenme -i' 
 
