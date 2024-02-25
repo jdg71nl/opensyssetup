87882c2fe7d907724c8fc7d406285202c297556d
John de Graaff <john@de-graaff.net>
2024-02-25 04:45:03 -0700

    some

---
+++ setup_scripts/rpi_remove_rfkill.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#=
+# (c)2023 John@de-Graaff.net
+# - - - - - - = = = - - - - - - . - - - - - - = = = - - - - - - .
+# display every line executed in this bash script:
+#set -o xtrace
+#
+BASENAME=`basename $0`
+echo "# running: $BASENAME ... "
+# SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
+# SCRIPT_PATH=`dirname $SCRIPT`
+#
+f_echo_exit1() { echo $1 ; exit 1 ; }
+if [ ! -e /etc/debian_version ]; then f_echo_exit1 "# Error: found non-Debain OS .." ; fi
+if ! which sudo >/dev/null ; then f_echo_exit1 "# please install first (as root) ==> apt install sudo " ; fi
+if ! which dpkg-query >/dev/null ; then f_echo_exit1 "# please install first: using ==> sudo apt install dpkg-query " ; fi
+#
+#usage() {
+#  #echo "# usage: $BASENAME { -req_flag | [ -opt_flag string ] } " 1>&2 
+#  echo "# usage: $BASENAME " 1>&2 
+#  exit 1
+#}
+#
+MYUID=$( id -u )
+if [ $MYUID != 0 ]; then
+  # https://unix.stackexchange.com/questions/129072/whats-the-difference-between-and
+  # $* is a single string, whereas $@ is an actual array.
+  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 0 ;
+fi
+# - - - - - - = = = - - - - - - . - - - - - - = = = - - - - - - .
+#
+#
+apt purge rfkill
+#
+#
+exit 0
+#
+#-eof
+
