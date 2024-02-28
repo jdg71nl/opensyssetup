153df75f10a290bf2a677c5b21398808041cd8ba
John de Graaff <john@de-graaff.net>
2024-02-28 16:46:14 +0100

    some

--- bin/p1.sh
+++ bin/p1.sh
@@ -2,11 +2,10 @@
 #= p1.sh | updated: d231108
 # (c)2023 John@de-Graaff.net
 #
-#
-echo "# > ping 1.1.1.1 ... "
 #ping -i3 1.1.1.1
-ping 1.1.1.1
 #
+echo "# > ping -4 1.1.1.1 ... "
+ping -4 1.1.1.1
 #
 exit 0
 #
