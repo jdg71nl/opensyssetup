f18daaf030492b9a66e5c5635ffceb4418023d72
John de Graaff <john@de-graaff.net>
2024-03-01 23:14:23 +0100

    some

--- bin/p1.sh
+++ bin/p1.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#= p1.sh | updated: d231108
-# (c)2023 John@de-Graaff.net
-#
-#ping -i3 1.1.1.1
-#
-echo "# > ping -4 1.1.1.1 ... "
-ping -4 1.1.1.1
-#
-exit 0
-#
+echo "# > cat /proc/sys/net/ipv4/ip_forward "
+cat /proc/sys/net/ipv4/ip_forward 
 #-eof
