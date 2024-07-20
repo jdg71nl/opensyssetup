06761baf0936999b8e340a54ef898e65bae436cf
John de Graaff <john@de-graaff.net>
2024-07-20 17:22:32 +0200

    some

---
+++ bin/sudopof.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#= sudopof == sudo poweroff
+#
+BASENAME=`basename $0`
+echo "# running: $BASENAME ... "
+#SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
+#SCRIPT_PATH=`dirname $SCRIPT`
+#
+MYUID=$( id -u )
+#usage() {
+#  #echo "# usage: $BASENAME { req.flag | [ -opt.flag string ] } " 1>&2 
+#  echo "# usage: $BASENAME " 1>&2 
+#  exit 1
+#}
+if [ $MYUID != 0 ]; then
+  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 0 ;
+fi
+#
+/usr/sbin/poweroff 
+#
+#-eof
