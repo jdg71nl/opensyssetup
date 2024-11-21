b289684ebd8da0e7f18cf4586dc8d06f7a5f864b
John de Graaff <john@de-graaff.net>
2024-11-21 15:01:54 +0100

    some

--- bin/ssh-copy-id_rsa-pub.sh
+++ bin/ssh-copy-id_rsa-pub.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #= ssh-copy-id_rsa-pub.sh 
 #
+echo "# running: ssh-copy-id_rsa-pub.sh ... "
+#
 # ssh-copy-id -i ~/.ssh/id_rsa.pub $1
+#
+echo "# running command: > ssh-copy-id -i ~/.ssh/id_rsa.pub $@"
 ssh-copy-id -i ~/.ssh/id_rsa.pub $@
 #
 #-eof
