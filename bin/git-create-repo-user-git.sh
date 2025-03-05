aac1abbb9d02647cff77cd7721ef4a9465f1a203
John de Graaff <john@de-graaff.net>
2025-03-05 16:43:18 +0000

    some

--- bin/git-create-repo-user-git.sh
+++ bin/git-create-repo-user-git.sh
@@ -8,10 +8,10 @@
 
 USER="git"
 DIR="/opt/git"
-#HOST="vps5.dgt-bv.com"
-HOST="jbase.j71.nl"
+#GITHOST="vps5.dgt-bv.com"
+GITHOST="jbase.j71.nl"
 PORT="2221"
-REPO="ssh://$USER@$HOST:$PORT$DIR"
+REPO="ssh://$USER@$GITHOST:$PORT$DIR"
 #ORIG="vps5"
 ORIG="jbase"
 BIN="/home/jdg/opensyssetup/bin/git-create-repo-user-git.sh"
@@ -19,24 +19,24 @@ BIN="/home/jdg/opensyssetup/bin/git-create-repo-user-git.sh"
 REPNAME="$1"
 REPDIR="$DIR/$REPNAME.git"
 
-#CMD="ssh -t -p $PORT jdg@$HOST 'REPO=\"RepoName\" ; /usr/local/syssetup/bin/git-create-repo-user-git.sh \$REPO' "
-#CMD="ssh -t -p $PORT jdg@$HOST '/usr/local/syssetup/bin/git-create-repo-user-git.sh $REPNAME'"
-#CMD="ssh -t -p $PORT jdg@$HOST '/usr/local/syssetup/bin/git-create-repo-user-git.sh repo-name '"
-#CMD="ssh -t -p $PORT jdg@$HOST '/bin/bash -c \"/usr/local/syssetup/bin/git-create-repo-user-git.sh $REPNAME\" '"
-#CMD="echo \"/usr/local/syssetup/bin/git-create-repo-user-git.sh $REPNAME\" | ssh -t -p $PORT jdg@$HOST '/bin/bash -l -s' "
-CMD="ssh -t -p $PORT jdg@$HOST '$BIN repo-name-without-.git '"
+#CMD="ssh -t -p $PORT jdg@$GITHOST 'REPO=\"RepoName\" ; /usr/local/syssetup/bin/git-create-repo-user-git.sh \$REPO' "
+#CMD="ssh -t -p $PORT jdg@$GITHOST '/usr/local/syssetup/bin/git-create-repo-user-git.sh $REPNAME'"
+#CMD="ssh -t -p $PORT jdg@$GITHOST '/usr/local/syssetup/bin/git-create-repo-user-git.sh repo-name '"
+#CMD="ssh -t -p $PORT jdg@$GITHOST '/bin/bash -c \"/usr/local/syssetup/bin/git-create-repo-user-git.sh $REPNAME\" '"
+#CMD="echo \"/usr/local/syssetup/bin/git-create-repo-user-git.sh $REPNAME\" | ssh -t -p $PORT jdg@$GITHOST '/bin/bash -l -s' "
+CMD="ssh -t -p $PORT jdg@$GITHOST '$BIN repo-name-without-.git '"
 
 BASENAME=`basename $0`
 usage() {
 	echo
-        echo "Usage: $BASENAME <repo-name>.git":
-        echo "  will create dir '/opt/git/repo-name.git/' and in it do 'git init --bare'"
+	echo "Usage: $BASENAME <repo-name>.git":
+	echo "  will create dir '/opt/git/repo-name.git/' and in it do 'git init --bare'"
 	echo
-        echo "  usage from 'git client':"
+	echo "  usage from 'git client':"
 	echo "  $CMD "
-        # echo "  ssh -t -p $PORT jdg@$HOST 'REPO=\"RepoName\" ; /usr/local/syssetup/bin/git-create-repo-user-git.sh repo-name '"
-        echo
-        #exit 1
+	# echo "  ssh -t -p $PORT jdg@$GITHOST 'REPO=\"RepoName\" ; /usr/local/syssetup/bin/git-create-repo-user-git.sh repo-name '"
+	echo
+	#exit 1
 }
 error_usage()  { echo "# $BASENAME: Error - $1"; usage       ; }
 error()        { echo "# $BASENAME: Error - $1"; exit 1      ; }
@@ -48,7 +48,7 @@ if [[ ! -d "$DIR" ]]; then
 	#
 	# echo "[running]> $CMD "
 	# # $CMD
-	# echo "/usr/local/syssetup/bin/git-create-repo-user-git.sh $REPNAME" | ssh -t -p $PORT jdg@$HOST '/bin/bash -l -s'
+	# echo "/usr/local/syssetup/bin/git-create-repo-user-git.sh $REPNAME" | ssh -t -p $PORT jdg@$GITHOST '/bin/bash -l -s'
 	# exit
 fi
 
@@ -56,7 +56,7 @@ fi
 [[ "$REPNAME" == "" ]]              && error_usage "provide new Repo name (without .git)"
 #[[ "$REPNAME" =~ "git" ]]          && error_usage "provide new repo name (WITHOUT .git)"
 echo "$REPNAME" | egrep -iq "\.git" && error_usage "provide new repo name (without .GIT)"
-[[ -d "$REPDIR" ]]                 && error "directory '$REPDIR' already exists!"
+[[ -d "$REPDIR" ]]                  && error "directory '$REPDIR' already exists!"
 
 # > visudo
 # #jdg
@@ -89,10 +89,21 @@ echo "# done!"
 echo
 echo "# next use these commands to add and setup remote:"
 cat <<HERE
+#
+# Note that this remote-repo is empty, so a 'git clone' will warn.
+# Best to do a small init commit:
+#
+git clone ssh://git@$$GITHOST:2221/opt/git/$REPNAME.git 
+touch README.txt
+git add README.txt 
+git commit -am "initial"
+git push
+#
 git-show-repos.sh 
-git remote add jbase ssh://git@$$HOST:2221/opt/git/$REPNAME.git 
+git remote add jbase ssh://git@$$GITHOST:2221/opt/git/$REPNAME.git 
 git push --set-upstream jbase main
 git branch --set-upstream-to=jbase/main main
+#
 HERE
 echo "# that's it folks!"
 
