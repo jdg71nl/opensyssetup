c0aacf54df267f25f0f4e60b06926e5a35d87c3a
John de Graaff <john@de-graaff.net>
2025-04-09 11:05:33 +0200

    some

---
+++ mac/bin/find_replace_gfind_mac.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# display every line executed in this bash script:
+#set -o xtrace
+#
+BASENAME=`basename $0`
+echo "# running: $BASENAME ... "
+#SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
+#SCRIPT_PATH=`dirname $SCRIPT`
+#echo "# now cd'ing (change dir) to:"
+#cd $SCRIPT_PATH
+#pwd
+#
+# https://en.wikipedia.org/wiki/Command-line_interface#Command_description_syntax
+# <angle>        brackets for required parameters:   ping <hostname>
+# [square]       brackets for optional parameters:   mkdir [-p] <dirname>
+# ellipses ...   for repeated items:                 cp <source1> [source2...] <dest>
+# vertical |     bars for choice of items:           netstat {-t|-u}
+#
+#usage() {
+#  #echo "# usage: $BASENAME { req.flag | [ -opt.flag string ] } " 1>&2 
+#  echo "# usage: $BASENAME " 1>&2 
+#  exit 1
+#}
+#echo_exit1() { echo $1 ; exit 1 ; }
+#
+#
+MYUID=$( id -u )
+if [ $MYUID != 0 ]; then
+  # $* is a single string, whereas $@ is an actual array.
+  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 0 ;
+fi
+#.
+
+GFIND="/opt/homebrew/bin/gfind"
+FIND="/usr/bin/find"
+FIND_BACKUP="$FIND.backup"
+
+if [ ! -f "$GFIND" ]; then
+  echo "# gfind not found: install using > brew install findutils "
+  exit 1
+fi
+
+if [ -f "$FIND_BACKUP" ]; then
+  echo "# file '$FIND_BACKUP' already exists ... "
+  exit 1
+fi
+
+echo "# executing ... "
+set -o xtrace
+
+mv $FIND $FIND_BACKUP
+
+cp -av $GFIND $FIND
+
+#-eof
+
+
+
